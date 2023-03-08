bf13a68062d8a6a4016b164bbc25538514d0af0f
yehezkiel ermanto <yehezkiel.ermanto28@gmail.com>
2023-03-08 21:29:27 +0700

    add sista landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./sista/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+content: ["./sista/**/*.{html,js}"],
+theme: {
+screens:{
+'2xs': '320px',
+'xs': '375px',
+'sm': '425px',
+// => @media (min-width: 640px) { ... }
+
+'md': '768px',
+// => @media (min-width: 768px) { ... }
+
+'lg': '1024px',
+// => @media (min-width: 1024px) { ... }
+
+'xl': '1280px',
+// => @media (min-width: 1280px) { ... }
+
+'2xl': '1536px',
+// => @media (min-width: 1536px) { ... }
+},
+extend: {},
+},
+plugins: [],
 }
